a1ff62d02455498ea255999f3a1706e2801487d4
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-10-12 19:08:02 +0900

    add StyledButton, StyledCard, and also enteredUserData state in AddUser

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import StyledCard from '@/components/UI/Card';
+import React, { useState } from 'react';
+import StyledCard from '@/components/UI/StyledCard';
 import styled from 'styled-components';
+import StyledButton from '@/components/UI/StyledButton';
 
 const StyledForm = styled.form`
   & label {
@@ -25,18 +26,42 @@ const StyledForm = styled.form`
 `;
 
 function AddUser(props) {
+  // Use user object for less state as much as possible.
+  const [enteredUserData, setEnteredUserData] = useState({ id: '', name: '', age: '' });
+
   const addUserHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    setEnteredUserData((prevState) => ({
+      ...prevState,
+      id: Math.random().toString(),
+    }))
+    console.log(enteredUserData)
+  };
+
+  const userNameChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredUserData((prevState) => ({
+      ...prevState,
+      name: event.target.value,
+    }));
+  };
+
+  const userAgeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredUserData((prevState) => ({
+      ...prevState,
+      age: event.target.value,
+    }));
   };
 
   return (
     <StyledCard>
       <StyledForm onSubmit={addUserHandler}>
         <label htmlFor="user-name">Username</label>
-        <input id="user-name" type="text" />
-        <label htmlFor="user-age">Age</label>
-        <input id="user-age" type="number" />
-        <button type='submit'>Add User</button>
+        <input id="user-name" type="text" onChange={userNameChangeHandler} />
+        <label htmlFor="user-age">Age (years)</label>
+        <input id="user-age" type="text" onChange={userAgeChangeHandler} />
+        <StyledButton type="submit">Add User</StyledButton>
       </StyledForm>
     </StyledCard>
   );
